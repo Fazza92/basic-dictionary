ce13bdbd03435faeb2320380d89b4e4b5659ad49
Oscar <oscarwasil@gmail.com>
2020-12-27 15:10:16 -0600

    Added word check to make sure word exists in data

--- basic-dictionary.py
+++ basic-dictionary.py
@@ -3,7 +3,10 @@ data = json.load(open("data.json"))
 
 
 def return_definition(word):
-  return data[word]
+  if word in data:
+    return data[word]
+  else:
+    return "Word does not exist. Please check again."
 
 word = input("Enter word: ")
 
