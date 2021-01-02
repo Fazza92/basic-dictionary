8a497526066885c05b4c5649c6f6beba79b62b1e
Oscar <oscarwasil@gmail.com>
2021-01-02 16:50:47 -0600

    Added a library to get close matches of word entered

--- basic-dictionary.py
+++ basic-dictionary.py
@@ -1,4 +1,6 @@
 import json
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
 
 
@@ -6,6 +8,8 @@ def return_definition(word):
   word = word.lower()
   if word in data:
     return data[word]
+  elif len(get_close_matches(w, data.keys())) > 0:
+    return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
   else:
     return "Word does not exist. Please check again."
 
